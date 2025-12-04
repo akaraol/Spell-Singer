0627e9bc8c733cd525f1363185c96748715c56c8
akaraol <90260540+akaraol@users.noreply.github.com>
2025-12-05 00:30:14 +0300

    ready for mig

--- SpellSinger/AppDbContext.cs
+++ SpellSinger/AppDbContext.cs
@@ -1,6 +1,24 @@
-﻿namespace SpellSinger
+﻿using Microsoft.EntityFrameworkCore;
+using MyAppName.Models;
+using SpellSinger.Domain;
+using System.Reflection;
+
+namespace MyAppName.Data;
+
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
 {
-    public class AppDbContext
+    public required DbSet<Spell> Spells { get; set; }
+    public required DbSet<DamageType> DamageTypes { get; set; }
+    public required DbSet<SourceBook> SourceBooks { get; set; }
+    public required DbSet<School> Schools { get; set; }
+    public required DbSet<ActionCategory> ActionCategories { get; set; }
+    public required DbSet<Class> Classes { get; set; }
+    public required DbSet<Condition> Conditions { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
     }
 }
--- SpellSinger/Domain/Spell.cs
+++ SpellSinger/Domain/Spell.cs
@@ -1,6 +1,34 @@
-﻿namespace SpellSinger.Domain
+﻿using SpellSinger.Domain;
+
+namespace MyAppName.Models;
+
+public class Spell : NamedEntity
 {
-    public class Spell
-    {
-    }
+    public int Level { get; set; }
+
+    public string? CastingTimeText { get; set; }
+    public string? Range { get; set; }
+    public string? Duration { get; set; }
+    public string? MaterialComponentDescription { get; set; }
+
+    public bool IsRitual { get; set; }
+    public bool IsConcentration { get; set; }
+    public bool HasVerbalComponent { get; set; }
+    public bool HasSomaticComponent { get; set; }
+    public bool HasMaterialComponent { get; set; }
+
+    public int SchoolId { get; set; }
+    public School? School { get; set; } 
+
+    public int ActionCategoryId { get; set; }
+    public ActionCategory? ActionCategory { get; set; }
+
+    public int SourceBookId { get; set; }
+    public SourceBook? SourceBook { get; set; }
+
+    public int? DamageTypeId { get; set; }
+    public DamageType? DamageType { get; set; }
+
+    public List<Class> Classes { get; set; } = [];
+    public List<Condition> Conditions { get; set; } = [];
 }
