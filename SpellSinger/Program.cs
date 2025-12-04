f4424bf125694bd0060eff395ec58e3372c66c05
akaraol <90260540+akaraol@users.noreply.github.com>
2025-12-05 00:30:43 +0300

    Update Program.cs

--- SpellSinger/Program.cs
+++ SpellSinger/Program.cs
@@ -1,15 +1,29 @@
+using Microsoft.EntityFrameworkCore; 
+using MyAppName.Data; 
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+
+AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
+    ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(connectionString));
+
+
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -24,6 +38,5 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}")
     .WithStaticAssets();
-
 
 app.Run();
